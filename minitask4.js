eb7acd571b61a01f6742367a5e0e6dd393d7163f
VirgilIw <imanuelambar11@gmail.com>
2025-09-04 14:04:10 +0700

    Add files via upload

--- minitask4.js
+++ minitask4.js
@@ -5,38 +5,38 @@
 
 // celcius ke fahrenheit
 
-function celciToFahren(suhu) {
-  let ceToF = (4 / 5) * suhu;
+function celciToFahren(celci) {
+  let ceToF = (4 / 5) * celci;
   return ceToF;
 }
 
 // celcius ke reamur
 
-const celciToReamur = function (suhu) {
-  let ceToR = (4 / 5) * suhu;
+const celciToReamur = function (celci) {
+  let ceToR = (4 / 5) * celci;
   return ceToR;
 };
 
 // celcius ke kelvin
 
-const celciToKelvin = (suhu) => {
-  let ceToK = (9 / 5) * suhu + 32;
+const celciToKelvin = (celci) => {
+  let ceToK = (9 / 5) * celci + 32;
   return ceToK;
 };
 
 // allOutput
 
-const allOutput = (suhu) => {
-  const cF = celciToFahren(suhu);
-  const cR = celciToReamur(suhu);
-  const cK = celciToKelvin(suhu);
+const allOutput = (celci) => {
+  const cF = celciToFahren(celci);
+  const cR = celciToReamur(celci);
+  const cK = celciToKelvin(celci);
 
   console.log(`Celcius ke Fahrenheit = ${cF}°F`);
   console.log(`Celcius ke Reamur = ${cR}°R`);
   console.log(`Celcius ke Kelvin = ${cK}°K`);
 };
 
-allOutput(20);
+allOutput(10);
 
 // kalau fungsi diluar kita isi argumen,
 // kalau makai variable, sesuaikan namanya lalu diisi di parameter fungsi
